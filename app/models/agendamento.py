aabf37648e89f15a446dad5c224df2da655af11f
Alceu Pereira <alceupereira3105@gmail.com>
2026-08-21 21:56:41 -0300

    feat: adiciona status com enum e restricao no banco ao Agendamento

--- app/models/agendamento.py
+++ app/models/agendamento.py
@@ -4,9 +4,10 @@ from app.models.cliente import Cliente
 
 from app.database import Base
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Enum as SQLEnum
 
 from sqlalchemy.orm import Mapped, mapped_column
+from app.models.enums import Estados
 
 class Agendamento(Base):
     __tablename__ = "agendamentos"
@@ -15,3 +16,4 @@ class Agendamento(Base):
     cliente_id: Mapped[int] = mapped_column(ForeignKey("clientes.id"))
     barbeiro_id: Mapped[int] = mapped_column(ForeignKey("barbeiros.id"))
     servico_id: Mapped[int] = mapped_column(ForeignKey("servicos.id"))
+    status: Mapped[Estados] = mapped_column(SQLEnum(Estados, create_constraint=True), default=Estados.PENDENTE)
---
+++ app/models/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Estados(Enum):
+    PENDENTE = "PENDENTE"
+    CONFIRMADO = "CONFIRMADO"
+    CANCELADO = "CANCELADO"
+    CONCLUIDO = "CONCLUIDO"
