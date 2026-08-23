1ef3e177f5b45fe0b4750ef88ca24824e9335aa4
Alceu Pereira <alceupereira3105@gmail.com>
2026-08-22 21:32:48 -0300

    feat: adiciona campos de data e preco snapshot ao Agendamento

--- app/models/agendamento.py
+++ app/models/agendamento.py
@@ -4,7 +4,9 @@ from app.models.cliente import Cliente
 
 from app.database import Base
 
-from sqlalchemy import ForeignKey, Enum as SQLEnum
+from sqlalchemy import ForeignKey, Enum as SQLEnum, DateTime, Numeric
+from datetime import datetime
+from decimal import Decimal
 
 from sqlalchemy.orm import Mapped, mapped_column
 from app.models.enums import Estados
@@ -17,3 +19,6 @@ class Agendamento(Base):
     barbeiro_id: Mapped[int] = mapped_column(ForeignKey("barbeiros.id"))
     servico_id: Mapped[int] = mapped_column(ForeignKey("servicos.id"))
     status: Mapped[Estados] = mapped_column(SQLEnum(Estados, create_constraint=True), default=Estados.PENDENTE)
+    data_hora_inicio: Mapped[datetime] = mapped_column(DateTime)
+    data_hora_fim: Mapped[datetime] = mapped_column(DateTime)
+    preco_cobrado: Mapped[Decimal] = mapped_column(Numeric(10, 2))
