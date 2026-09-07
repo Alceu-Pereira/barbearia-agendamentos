682672e629bd0a011148fe433e4cce8e8951a175
Alceu Pereira <alceupereira3105@gmail.com>
2026-09-07 18:51:16 -0300

    feat: rota de listagem de disponibilidade via query params

--- app/routers/agendamento.py
+++ app/routers/agendamento.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, HTTPException, Depends
 
 from app.schemas.agendamento import AgendamentoCreate, AgendamentoResponse
 
-from app.services.agendamento_service import criar_agendamento
+from app.services.agendamento_service import criar_agendamento, listar_disponibilidade
 
 from app.services.exceptions import RecursoNaoEncontrado, ConflitoDeHorario, OperacaoInvalida
 
 from sqlalchemy.orm import Session
 from app.database import get_db
+from datetime import date, datetime
 
 router = APIRouter(
     prefix="/api/v1/agendamentos",
@@ -25,3 +26,11 @@ def criar_agendamento_router(dados: AgendamentoCreate, db: Session = Depends(get
 
     return agendamento
 
+@router.get("/disponibilidade", response_model=list[datetime])
+def listar_disponibilidade_router(barbeiro_id: int, data: date, db: Session = Depends(get_db)):
+    try:
+        horarios_disponiveis = listar_disponibilidade(db, barbeiro_id, data)
+    except RecursoNaoEncontrado:
+        raise HTTPException(status_code=404, detail="Barbeiro não encontrado.")
+
+    return horarios_disponiveis
