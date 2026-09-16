3be74e031c2ac391f59da241cc56a65758d6c2cb
Alceu Pereira <alceupereira3105@gmail.com>
2026-09-15 21:12:29 -0300

    test: testes do router de Cliente (criar, buscar existente, buscar inexistente)

--- tests/test_cliente_router.py
+++ tests/test_cliente_router.py
@@ -10,3 +10,26 @@ def test_criar_cliente(client):
         "telefone": "5512345678",
         "ativo": True,
     }
+
+def test_buscar_cliente_por_id_inexistente(client):
+    response = client.get(
+       "/api/v1/clientes/9999"
+    )
+    assert response.status_code == 404
+    assert response.json() == { "detail": "Cliente não encontrado" }
+
+def test_buscar_cliente_por_id_existente(client):
+    cliente = client.post(
+        "/api/v1/clientes",
+        json={"nome": "Teste", "telefone": "5512345678"},
+    )
+    response = client.get(
+        f"/api/v1/clientes/{cliente.json()["id"]}"
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": cliente.json()["id"],
+        "nome": "Teste",
+        "telefone": "5512345678",
+        "ativo": True,
+        }
